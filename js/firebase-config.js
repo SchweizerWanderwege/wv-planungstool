47920d7aead80c8224d4daaa5b28b468895ace44
SchweizerWanderwege <92431739+SchweizerWanderwege@users.noreply.github.com>
2025-08-22 17:37:33 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDG-B0RENmdHuZ5wkwnA7_CK3If8dRHGEY",
   authDomain: "wv-planungstool.firebaseapp.com",
+  databaseURL: "https://wv-planungstool-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "wv-planungstool",
   storageBucket: "wv-planungstool.firebasestorage.app",
   messagingSenderId: "879920840021",
   appId: "1:879920840021:web:893409be06f6ad7ae903dc",
-  measurementId: "G-F5T4LN47W8",
-  databaseURL: "https://wv-planungstool-default-rtdb.europe-west1.firebasedatabase.app"
+  measurementId: "G-F5T4LN47W8"  
 };
 
 firebase.initializeApp(firebaseConfig);
