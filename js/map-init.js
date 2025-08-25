2de45c2fc11f0cd06ca191b6b6591b6ffc656bcd
SchweizerWanderwege <92431739+SchweizerWanderwege@users.noreply.github.com>
2025-08-25 16:07:58 +0200

    Update map-init.js

--- js/map-init.js
+++ js/map-init.js
@@ -30,12 +30,47 @@ const geojsonLayer = new ol.layer.Vector({
   })
 });
 
+var wms_layers = [];
+
+
+    var projection_NationalMapscolor_0 = ol.proj.get('EPSG:3857');
+    var projectionExtent_NationalMapscolor_0 = projection_NationalMapscolor_0.getExtent();
+    var size_NationalMapscolor_0 = ol.extent.getWidth(projectionExtent_NationalMapscolor_0) / 256;
+    var resolutions_NationalMapscolor_0 = new Array(14);
+    var matrixIds_NationalMapscolor_0 = new Array(14);
+    for (var z = 0; z < 25; ++z) {
+        // generate resolutions and matrixIds arrays for this WMTS
+        resolutions_NationalMapscolor_0[z] = size_NationalMapscolor_0 / Math.pow(2, z);
+        matrixIds_NationalMapscolor_0[z] = z;
+    }
+    var lyr_NationalMapscolor_0 = new ol.layer.Tile({
+                            source: new ol.source.XYZ(({
+                                url: "https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
+                                attributions: '© <a href="https://www.swisstopo.admin.ch/">Swisstopo</a>',
+
+              projection: projection_NationalMapscolor_0,
+              tileGrid: new ol.tilegrid.WMTS({
+                origin: ol.extent.getTopLeft(projectionExtent_NationalMapscolor_0),
+                resolutions: resolutions_NationalMapscolor_0,
+                matrixIds: matrixIds_NationalMapscolor_0
+              }),
+              style: 'ch.swisstopo.pixelkarte-farbe',
+              wrapX: true,
+                                "VERSION": "1.0.0",
+                            })),
+                            title: 'Landeskarte',
+                            opacity: 1.0,
+                            
+                            
+                          });
+
 // Map
 map = new ol.Map({
   target: 'map',
   layers: [
-    new ol.layer.Tile({ source: new ol.source.OSM() }),
-    vectorLayer, geojsonLayer
+    lyr_NationalMapscolor_0,
+    vectorLayer,
+    geojsonLayer
   ],
   view: new ol.View({
     center: ol.proj.fromLonLat([8.3, 46.8]),
@@ -58,4 +93,5 @@ map.addOverlay(overlay);
 
 // Filter initialisieren
 initFilters(geojsonSource, vectorLayer);
+
 
